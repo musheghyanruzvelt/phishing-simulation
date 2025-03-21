ab101612007891158e0e4add7f9a4641bc2ba22c
musheghyanruzvelt <ruzveltmusheghyan@gmail.com>
2025-03-21 15:14:57 +0400

    feat(config-service): setup

--- packages/attempts-management-service/src/main.ts
+++ packages/attempts-management-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/attempts');
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('PORT', 3002);
+
+  await app.listen(PORT);
+
+  logger.log(`Management Server is running on http://localhost:${PORT}`);
 }
+
 bootstrap();
--- packages/phishing-simulation-service/src/main.ts
+++ packages/phishing-simulation-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/simulation');
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('PORT', 3002);
+
+  await app.listen(PORT);
+
+  logger.log(`Simulation service is running on http://localhost:${PORT}`);
 }
 bootstrap();
